aebf98a18bd6e9671df4e8709815b688f028ac8c
Madison Bentley <mb.madisonbentley@gmail.com>
2020-04-13 22:18:27 -0700

    refactored phonebook project

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,14 +1,12 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
-  const [ newName, setNewName ] = useState('')
 
-  const [ newNumber, setNewNumber ] = useState('')
 
+const Header = ({text}) => {
+  return <h2>{text}</h2>
+}
+const PersonForm = ({persons, setPersons, setNewName, setNewNumber, handleChange, newNumber, newName, handleNumChange}) =>{
   const handleSubmit = (event) =>{
     let isduplicate = false;
     event.preventDefault();
@@ -29,40 +27,73 @@ const App = () => {
     }
   }
 
+  return(
+  <form onSubmit = {handleSubmit}>
+    <div>
+      name: <input name="name" value={newName} onChange = {handleChange}/>
+    </div>
+    <div>
+      number: <input name="num" value={newNumber} onChange = {handleNumChange}/>
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>)
+}
+const PeopleList = ({notesToShow, persons}) => {
+  return(
+    <ul>
+      {notesToShow.map(person =>
+        <li key = {person.name}>{person.name} - {person.number}</li>)}
+    </ul>
+  )
+}
+const Filter = ({setFilterName, persons, filterName}) => {
+  const handleFilterChange = (event) =>{
+    setFilterName(event.target.value)
+  }
+  return(
+    <>
+      <p>filter shown with</p>
+      <input name="filterName" value={filterName} onChange={handleFilterChange}/>
+    </>
+  )
+
+}
+const App = () => {
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filterName, setFilterName ] = useState('')
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const notesToShow = filterName.length == 0
+  ? persons : persons.filter(person => person.name.includes(filterName))
   const handleChange = (event) =>{
-    console.log(event.target.value);
     setNewName(event.target.value)
   }
-
   const handleNumChange = (event) => {
     setNewNumber(event.target.value)
   }
 
-  const PeopleList = ({persons}) => {
-    return(
-      <ul>
-        {persons.map(person =>
-          <li key = {person.name}>{person.name} - {person.number}</li>)}
-      </ul>
-    )
-
-  }
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit = {handleSubmit}>
-        <div>
-          name: <input name="name" value={newName} onChange = {handleChange}/>
-        </div>
-        <div>
-          number: <input name="num" value={newNumber} onChange = {handleNumChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <PeopleList persons={persons}/>
+      <Header text= "Phonebook"/>
+      <Filter persons= {persons} setFilterName = {setFilterName} />
+      <Header text="Add new"/>
+      <PersonForm
+        persons={persons}
+        setFilterName={setFilterName}
+        newName = {newName}
+        newNumber={newNumber}
+        handleNumChange={handleNumChange}
+        handleChange={handleChange}
+      />
+      <Header text="Numbers"/>
+      <PeopleList notesToShow={notesToShow} persons={persons}/>
     </div>
   )
 }
