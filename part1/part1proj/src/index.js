fe7378fee50591f43d4e60e6cd48e6b9f77cb9fd
Madison Bentley <mb.madisonbentley@gmail.com>
2020-04-20 22:55:45 -0700

    trying to push updated content

--- part1/part1proj/src/index.js
+++ part1/part1proj/src/index.js
@@ -1,10 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import Course from './components/Course.js';
-
-
+import Note from './components/Note.js';
+import axios from 'axios';
 
 const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
+  return(<></>)
+}
+/*const App = () => {
   const courses = [
     {
       name: 'Half Stack application development',
@@ -49,13 +65,19 @@ const App = () => {
       ]
     }
   ]
+  let notes = [];
+  axios.get('http://localhost:3001/notes')
+       .then(response => {
+         notes = response.data
+         console.log(notes)
+     })
   return(
       <div>
         {courses.map(course => <Course key={course.id} course={course} /> )}
       </div>
     )
 
-  }
+  }*/
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ part2/phonebookpt3/precache-manifest.35dc8d148da70e6b78c93460fbe4390c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d303d672f730aa2a873628c635d935e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d59f6d66657dc2ce018",
+    "url": "/static/css/main.4ff1733b.chunk.css"
+  },
+  {
+    "revision": "3388d186a0192b97de22",
+    "url": "/static/js/2.94cdbaa7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94cdbaa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d59f6d66657dc2ce018",
+    "url": "/static/js/main.a0b7d963.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
